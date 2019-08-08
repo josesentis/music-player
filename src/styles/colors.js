6b52f181cdf908203c9748cb1f9347fde35ffd6c
Jose Sentis <jose.sentis1987@gmail.com>
2019-08-08 19:39:43 +0200

    Adds new colors and style mixins / helpers

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,17 +1,24 @@
+import { bgGradient } from './mixins/helpers';
+
 const colors = {
   basics: {
     black: '#000',
     white: '#fff'
   },
   neutro: {
-    base: '#ACC3DB',
-    dark: '#8A8B8E',
-    darker: '#55575B',
-    darkest: '#25282C',
-    light: '#D0DDEB',
-    lighter: '#E4EBF4',
-    lightest: '#F8F9FD'
+    100: '#FCF9F9',
+    300: '#FFD8DD',
+    500: '#7869E5',
+    700: '#25234c',
+    900: '#0c0421'
+  },
+  gradient: {
+    primary: '#3023AE',
+    secondary: '#C96DD8'
   }
 };
 
+const gradient = bgGradient('166deg', colors.gradient.primary, colors.gradient.secondary);
+
+export { gradient };
 export default colors;
---
+++ src/styles/mixins/rhythm.js
@@ -0,0 +1,32 @@
+import { typography } from '..';
+
+/**
+ * Modular Scale
+ * Returns the modular scale size in rems
+ * https://www.modularscale.com/?1&em&1.067
+ *
+ * @param {integer} n â€” Exponencial value
+ */
+const modular = n => `${Math.pow(typography.fontRatio, n) || 1}rem`;
+
+/**
+ * Rems
+ * Transforms pixels into rems based in the base-font-size set in the theme
+ * file
+ *
+ * @param {integer|string} n â€” Number to transform
+ */
+const rems = n => `${parseInt(n, 10) / typography.fontSize}rem`;
+
+const pixelate = n => `${n}px`;
+
+/**
+ * Space
+ * Vertical and Horizontal Rhythm generator based on the base-line-height set in
+ * the theme file
+ *
+ * @param {float} n â€” Multiplier, can accept decimal numbers
+ */
+const space = (n = 1) => `${typography.lineHeight * n}rem`;
+
+export { space, pixelate, rems, modular };
