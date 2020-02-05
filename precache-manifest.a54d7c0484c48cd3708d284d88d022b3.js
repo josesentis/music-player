7c06b12f066a9c0ba281df6ced9df504b5e3b6fa
Jose Sentis <jose.sentis1987@gmail.com>
2020-02-05 21:17:27 +0100

    Updates

---
+++ precache-manifest.a54d7c0484c48cd3708d284d88d022b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf05225dfba76058eef5148b008628dd",
+    "url": "/music-player/index.html"
+  },
+  {
+    "revision": "0947f9445a48911be154",
+    "url": "/music-player/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "0947f9445a48911be154",
+    "url": "/music-player/static/js/2.81345d2b.chunk.js"
+  },
+  {
+    "revision": "5b1354d31a844c4a3ca2",
+    "url": "/music-player/static/js/main.3e3453b4.chunk.js"
+  },
+  {
+    "revision": "ae1bece6dc169c4ab370",
+    "url": "/music-player/static/js/runtime~main.fdd2de90.js"
+  }
+]);
