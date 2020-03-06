74cf2270b7364b4a44d20f8ee19009a2921c6262
Jose Sentis <jose.sentis1987@gmail.com>
2020-03-06 20:59:52 +0100

    Update utilities.js
    Removes comments

--- src/styles/utilities.js
+++ src/styles/utilities.js
@@ -14,62 +14,6 @@ const Tag = `
   }
 `;
 
-// const CommonHeadingStyles = `
-//   margin-bottom: ${space()};
-
-//   & + h1, & + h2, & + h3 {
-//     margin-top: ${space(0.5)};
-//   }
-// `;
-
-// const Titles = `
-//   .t-display,
-//   .t-title1,
-//   .t-title4 {
-//     ${CommonHeadingStyles}
-//   }
-
-//   .t-dashboard {
-//     font-size: ${modular(7)};
-//     font-weight: 300;
-//     letter-spacing: .3px;
-//     line-height: ${space(2.25)};
-
-//     ${media.min('desktopLarge')`
-//       font-size: ${modular(8)};
-//       line-height: ${space(2.75)};
-//     `}
-//   }
-
-//   .t-title1 {
-//     font-size: ${modular(4)};
-//     font-weight: 800;
-//     letter-spacing: .3px;
-//     line-height: ${space(1.75)};
-
-//     ${media.min('desktopLarge')`
-//       font-size: ${modular(5)};
-//       line-height: ${space(2)};
-//     `}
-//   }
-
-//   .t-title2 {
-//     ${Title2}
-//   }
-
-//   .t-title3 {
-//     ${Title3};
-//     margin-bottom: ${space(0.5)};
-//   }
-
-//   .t-title4 {
-//     ${Subtitle};
-//     color: ${colors.neutro.dark};
-//     font-weight: 400;
-//     margin-bottom: ${space(0.5)};
-//   }
-// `;
-
 const Utilities = `
   ${Tag}
 `;
